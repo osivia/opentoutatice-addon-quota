8c12be5af98a7fdc2a5edb98fccb413b0db83ca4
Jean-Sébastien <Jean-Sbastien@DESKTOP-IKBRQGB>
2019-06-07 17:37:33 +0200

    mise en place blob sur opération

--- src/main/java/org/opentoutatice/addon/quota/automation/QuotaInfo.java
+++ src/main/java/org/opentoutatice/addon/quota/automation/QuotaInfo.java
@@ -8,10 +8,15 @@ import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentRef;
+import org.nuxeo.ecm.core.api.impl.blob.StringBlob;
 import org.opentoutatice.addon.quota.check.util.BlobsSizeComputer;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 /**
  * @author dchevrier <chevrier.david.pro@gmail.com>
@@ -28,11 +33,18 @@ public class QuotaInfo {
 	protected CoreSession session;
 
 	@OperationMethod
-	public String run(DocumentRef docRef) throws ClientException {
+	public Object run(DocumentRef docRef) throws ClientException {
+		
+        JSONObject QuotaItems = new JSONObject();
 
 		Long treeSize = BlobsSizeComputer.get().getTreeSizeFrom(this.session, docRef);
-
-		return treeSize != null ? String.valueOf(treeSize) : null;
+		QuotaItems.put("treesize", treeSize);
+ 
+        return createBlob(QuotaItems);
 	}
+	
+    private Blob createBlob(JSONObject json) {
+        return new StringBlob(json.toString(), "application/json");
+    }
 
 }
--- src/main/java/org/opentoutatice/addon/quota/check/exception/QuotaExceededException.java
+++ src/main/java/org/opentoutatice/addon/quota/check/exception/QuotaExceededException.java
@@ -4,17 +4,19 @@
 package org.opentoutatice.addon.quota.check.exception;
 
 import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.DocumentSecurityException;
+import org.nuxeo.ecm.core.api.RecoverableClientException;
 
 /**
  * @author dchevrier <chevrier.david.pro@gmail.com>
  *
  */
-public class QuotaExceededException extends ClientException {
+public class QuotaExceededException extends RecoverableClientException {
 
 	private static final long serialVersionUID = 4947353778360167920L;
 
 	public QuotaExceededException(Long quota_) {
-		super(String.format("Quota exceeded (%d bytes)", quota_));
+		super("Quota exceeded", String.format("Quota exceeded (%d bytes)", quota_), new String[] {});
 	}
 	
 }
